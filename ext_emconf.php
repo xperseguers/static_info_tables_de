c42f22da10aff2a2e8c7a6c6cfe51b4549d64903
Helmut Hummel <helmut.hummel@typo3.org>
2014-07-31 11:09:37 +0200

    latest version

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "static_info_tables_de".
+ *
+ * Auto generated 31-07-2014 11:07
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Static Info Tables (de)',
+	'description' => 'German (de) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'version' => '6.0.1',
+	'state' => 'stable',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Daniel Minder',
+	'author_email' => 'typo3@minder.de',
+	'author_company' => '',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
